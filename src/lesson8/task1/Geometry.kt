240c8ed5805eb8d5c2b7ec44388931bb9c5d6331
OGSegu <seguisreal@gmail.com>
2019-12-18 18:04:35 +0300

    Some hard exercises

--- src/lesson8/task1/Geometry.kt
+++ src/lesson8/task1/Geometry.kt
@@ -158,7 +158,11 @@ class Line(val b: Double, val angle: Double) {
      * Найти точку пересечения с другой линией.
      * Для этого необходимо составить и решить систему из двух уравнений (каждое для своей прямой)
      */
-    fun crossPoint(other: Line): Point = TODO()
+    fun crossPoint(other: Line): Point =
+        Point(
+        (other.b * cos(angle) - b * cos(other.angle)) / sin(angle - other.angle),
+        (other.b * sin(angle) - b * sin(other.angle)) / sin(angle - other.angle)
+    )
 
     override fun equals(other: Any?) = other is Line && angle == other.angle && b == other.b
 
@@ -220,7 +224,10 @@ fun findNearestCirclePair(vararg circles: Circle): Pair<Circle, Circle> = TODO()
  * (построить окружность по трём точкам, или
  * построить окружность, описанную вокруг треугольника - эквивалентная задача).
  */
-fun circleByThreePoints(a: Point, b: Point, c: Point): Circle = TODO()
+fun circleByThreePoints(a: Point, b: Point, c: Point): Circle {
+    val center =  bisectorByPoints(a, b).crossPoint(bisectorByPoints(b,c))
+    return Circle(center,center.distance(a))
+}
 
 /**
  * Очень сложная
